7326b7f1f5ef6ee6cce4a391579b6852249e7dd4
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2023-10-06 19:19:09 +0600

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let arr = []; 
+  let dis = b ** 2 - 4 * a * c;
+
+  if(dis === 0){
+   let cor = -b / ( 2 * a );
+   arr.push(cor)
+   return arr;
+  } else if(dis > 0){
+    let cor1 = (-b + Math.sqrt(dis))/(2*a);
+    let cor2 = (-b - Math.sqrt(dis))/(2*a);
+    arr.push(cor1, cor2)
+    return arr;
+  } else if(dis < 0){
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
