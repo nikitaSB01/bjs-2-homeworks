c68aeecde11f7f238a562a0d75ae3724de75a522
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2023-10-07 20:48:54 +0600

    первое задание выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,22 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    } else if(arr[i] < min){
+      min = arr[i];
+    }
+    sum += arr[i]
+    avg = Number((sum / arr.length).toFixed(2))
+
+  }
 
   return { min: min, max: max, avg: avg };
 }
+//getArrayParams(5, 7, 1)
+
 
 function summElementsWorker(...arr) {
 
