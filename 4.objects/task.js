7a15e314d84a78e220d7968018d50c204bd9251a
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2023-10-15 22:41:17 +0600

    сделал домашку

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+/* let student1 = new Student("ivan", "мужской", "20");
+let student2 = new Student("lena", "женский", "25");
+let student3 = new Student("ivan", "мужской", "10"); */
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if ("marks" in this) {
+    this.marks.push(...marks);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if ("marks" in this === false || this.marks.length <= 0) {
+     return 0;
+  } else {
+    return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+  }
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
